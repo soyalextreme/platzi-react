f544269a96b39eb036a5abc418f6a05a728f4e3b
soyalextreme <soyalextreme@gmail.com>
2021-02-02 03:12:53 -0600

    [add] Api [add] Fetch of the information [add] Props for the Carousel Item

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
@@ -10,27 +10,46 @@ import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
 
 
-const App = () => (
+
+const App = () => {
+
+    useEffect(() => {
+
+        const fetchVideos = () => {
+            const API = "http://localhost:3000/initalState";
+            fetch(API).then(response => response.json()).then(data => setVideos(data));
+        }
+
+        fetchVideos();
+
+    }, [])
+
+    
+    const [videos, setVideos] = useState({mylist: [], originals: [], trends: []});
+
+    
+    return (
     <div className="app">
         <Header />
         <Search />
         <Categories titleCategory="Mi Lista">
             <Carousel>
-                <CarouselItem />
+                {videos.mylist.length > 0 && videos.mylist.map( data => <CarouselItem key={data.id} {...data} />) }
             </Carousel>
         </Categories>
         <Categories titleCategory="Originales Platzi">
             <Carousel>
-                <CarouselItem />
+                {videos.originals.length > 0 && videos.originals.map( data => <CarouselItem key={data.id} {...data} />)}
             </Carousel>
         </Categories>
         <Categories titleCategory="Tendencias">
             <Carousel>
-                <CarouselItem />
+                {videos.trends.length > 0 && videos.trends.map( data => <CarouselItem key={data.id} {...data} />)}
             </Carousel>
         </Categories>
         <Footer />
     </div>
-);
+    );
+}
 
 export default App;
